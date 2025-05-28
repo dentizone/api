1fa9d8956f783669e2246d17df9787e611b74982
Yara Nagy <yara1998@live.com>
2025-05-28 12:12:49 +0300

    S2

---
+++ Dentizone.Domain/Enums/WithdrawalRequestStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentizone.Domain.Enums
+{
+   public enum WithdrawalRequestStatus
+    {
+        Pending,    // Request has been made but not yet processed
+        Approved,   // Request has been approved and is being processed
+        Rejected,   // Request has been rejected
+        Completed,  // Withdrawal has been successfully completed
+        Failed      // Withdrawal failed due to some error
+    }
+}
--- Dentizone.Infrastructure/Persistence/Configurations/PostConfiguration.cs
+++ Dentizone.Infrastructure/Persistence/Configurations/PostConfiguration.cs
@@ -14,50 +14,10 @@ namespace Dentizone.Infrastructure.Persistence.Configurations
     {
         public void Configure(EntityTypeBuilder<Post> builder)
         {
-
-            builder.HasKey(p => p.Id);
-            builder.Property(p => p.Id)
-                .IsRequired();
-            builder.Property(p => p.title)
-                .HasMaxLength(255)
-                .IsRequired();
-            builder.Property(p => p.description)
-                .HasMaxLength(-1)
-                .IsRequired();
-            builder.Property(p => p.price)
-                .IsRequired()
-                .HasColumnType("decimal(18,2)");
-            builder.Property(p => p.expire_date)
-                .IsRequired(false);
-
-
-            builder.Property(p => p.condition)
-                .IsRequired()
-                .HasConversion<string>();
-
-
-            builder.Property(p => p.status)
-                .IsRequired();
-            builder.Property(p => p.seller_id)
-                .IsRequired();
-            builder.Property(p => p.item_id)
-                .IsRequired();
-            builder.Property(p => p.slug)
-                .HasMaxLength(255)
-                .IsRequired();
-            builder.Property(p => p.CreatedAt)
-                .HasDefaultValueSql(SQLCommon.Date)
-                .ValueGeneratedOnAdd();
-
-
-            builder.Property(p => p.UpdatedAt)
-                .IsRequired();
-            builder.Property(p => p.IsDeleted)
-                .IsRequired();
-
-    
-
+            
         }
     }
 }
+
+
 
