6d3ec2c7e00de363cffa9216f3c7ba37f6338497
Mahmoud Nasr <239.nasr@gmail.com>
2025-05-29 16:22:05 +0300

    Refactor IBaseRepo interface and clean up using directives
    Removed unnecessary using directives and simplified the IBaseRepo interface by eliminating the GetAllAsync and UpdateAsync method signatures. The overall structure remains intact with fewer methods.

--- Dentizone.Application/Interfaces/IBaseRepo.cs
+++ Dentizone.Application/Interfaces/IBaseRepo.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dentizone.Application.Interfaces
+﻿namespace Dentizone.Application.Interfaces
 {
     internal interface IBaseRepo<TEntity> where TEntity : class
     {
         Task<TEntity> GetByIdAsync(int id);
-
         Task<IEnumerable<TEntity>> GetAllAsync(int page = 1);
         Task<TEntity> CreateAsync<TC>(TC entity);
-        Task<TEntity> UpdateAsync<TU>(int id, TU UpdatePayload);
         Task<TEntity> DeleteAsync(int id);
     }
 }
