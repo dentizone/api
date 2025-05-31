74e75e86b33ee2b4207dd9df82bae3f5b26553fd
Mahmoud Nasr <239.nasr@gmail.com>
2025-05-31 23:13:09 +0300

    Enhance OrderStatus management with repository pattern
    - Initialize Id property in OrderStatus with a new GUID.
    - Define IOrderStatusRepository interface for repository operations.
    - Implement OrderStatusRepository with async methods for CRUD operations.

---
+++ Dentizone.Application/Interfaces/IOrderStatusRepository.cs
@@ -0,0 +1,8 @@
+﻿using Dentizone.Application.Interfaces;
+using Dentizone.Domain.Entity;
+
+namespace Dentizone.Application.Repositories;
+
+interface IOrderStatusRepository : IBaseRepo<OrderStatus>
+{
+}
---
+++ Dentizone.Application/Repositories/OrderStatusRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dentizone.Application.Abstracts;
+using Dentizone.Domain.Entity;
+using Dentizone.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dentizone.Application.Repositories;
+
+public class OrderStatusRepository : AbstractRepository, IOrderStatusRepository
+{
+    public OrderStatusRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<OrderStatus?> GetByIdAsync(string id)
+    {
+        return await dbContext.OrderStatuses
+                              .FirstOrDefaultAsync(o => o.Id == id && !o.IsDeleted);
+    }
+
+    public async Task<IEnumerable<OrderStatus>> GetAllAsync(int page = 1)
+    {
+        return await dbContext.OrderStatuses
+                              .Where(o => !o.IsDeleted)
+                              .Skip(CalculatePagination(page))
+                              .Take(DefaultPageSize)
+                              .ToListAsync();
+    }
+
+    public async Task<OrderStatus> CreateAsync(OrderStatus entity)
+    {
+        await dbContext.OrderStatuses.AddAsync(entity);
+        await dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<OrderStatus?> DeleteAsync(string id)
+    {
+        var toBeDeleted = await GetByIdAsync(id);
+        if (toBeDeleted == null)
+        {
+            return null;
+        }
+
+        dbContext.OrderStatuses.Remove(toBeDeleted);
+        await dbContext.SaveChangesAsync();
+        return toBeDeleted;
+    }
+}
