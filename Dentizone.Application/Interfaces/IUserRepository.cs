cca92f1e6f42b85140d3a878bef1b47f28bc63da
Mahmoud Nasr <239.nasr@gmail.com>
2025-05-31 22:34:48 +0300

    Enhance user management with AppUser and repository updates
    - Modified `AppUser` to implement `IBaseEntity` and initialize `Id` with a GUID.
    - Updated `IAppUser` interface to simplify structure by removing `IBaseEntity` inheritance.
    - Introduced `IUserRepository` interface for user management, including an update method.
    - Created `UserRepository` class to implement `IUserRepository`, providing async methods for CRUD operations on `AppUser`.

---
+++ Dentizone.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Dentizone.Domain.Entity;
+
+namespace Dentizone.Application.Interfaces;
+
+internal interface IUserRepository : IBaseRepo<AppUser>
+{
+    Task<AppUser> Update(AppUser entity);
+}
