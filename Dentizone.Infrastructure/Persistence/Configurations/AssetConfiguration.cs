1c0e65a4b5dac2099f3d29891bbfad864a4f947f
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-03 20:09:50 +0300

    Enhance AssetConfiguration with new properties and annotations
    Updated AssetConfiguration to include additional properties and annotations for the Asset entity. Added a using directive for System.ComponentModel.DataAnnotations, introduced range validation for the Size property, and ensured CreatedAt, UpdatedAt, Status, and IsDeleted properties are required with appropriate default values. Adjusted relationship comments and definitions for clarity and consistency.

--- Dentizone.Infrastructure/Persistence/Configurations/AssetConfiguration.cs
+++ Dentizone.Infrastructure/Persistence/Configurations/AssetConfiguration.cs
@@ -2,58 +2,59 @@
 using Dentizone.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dentizone.Infrastructure.Persistence.Configurations
 {
-    class AssetConfiguration : IEntityTypeConfiguration<Asset>
-    {
-        public void Configure(EntityTypeBuilder<Asset> builder)
-        {
-            builder.HasKey(a => a.Id);
+       class AssetConfiguration : IEntityTypeConfiguration<Asset>
+       {
+              public void Configure(EntityTypeBuilder<Asset> builder)
+              {
+                     builder.HasKey(a => a.Id);
 
-            builder.Property(a => a.Url)
-                   .IsRequired()
-                   .HasMaxLength(2048);
+                     builder.Property(a => a.Url)
+                            .IsRequired()
+                            .HasMaxLength(2048);
 
-            builder.Property(a => a.Size)
-                   .IsRequired();
+                     builder.Property(a => a.Size)
+                            .IsRequired()
+                            .HasAnnotation("Range", new RangeAttribute(1, 104857600));
+                     builder.Property(a => a.Type)
+                            .IsRequired()
+                            .HasConversion<string>();
 
-            builder.Property(a => a.Type)
-                   .IsRequired()
-                   .HasConversion<string>();
+                     builder.Property(a => a.CreatedAt)
+                            .ValueGeneratedOnAdd()
+                            .IsRequired()
+                            .HasDefaultValueSql(SqlCommon.Date);
 
-            builder.Property(a => a.CreatedAt)
-                   .ValueGeneratedOnAdd()
-                   .IsRequired()
-                   .HasDefaultValueSql(SqlCommon.Date);
+                     builder.Property(a => a.UpdatedAt)
+                            .IsRequired().HasDefaultValueSql(SqlCommon.Date);
 
-            builder.Property(a => a.UpdatedAt)
-                   .IsRequired().HasDefaultValueSql(SqlCommon.Date);
+                     builder.Property(a => a.Status)
+                            .IsRequired()
+                            .HasDefaultValue(AssetStatus.Active)
+                            .HasConversion<string>();
 
-            builder.Property(a => a.Status)
-                   .IsRequired()
-                   .HasDefaultValue(AssetStatus.Active)
-                   .HasConversion<string>();
-
-            builder.Property(a => a.IsDeleted)
-                   .IsRequired()
-                   .HasDefaultValue(false);
-
-
-            // --- Relationships ---
+                     builder.Property(a => a.IsDeleted)
+                            .IsRequired()
+                            .HasDefaultValue(false);
 
 
-            // One-to-Many: Asset to UserAssets
-            builder.HasMany(a => a.UserAssets)
-                   .WithOne(ua => ua.Asset)
-                   .HasForeignKey(ua => ua.AssetId)
-                   .OnDelete(DeleteBehavior.Cascade);
+                     // --- Relationships ---
 
 
-            // One-to-Many: Asset to PostAssets
-            builder.HasMany(a => a.PostAssets)
-                   .WithOne(pa => pa.Asset)
-                   .HasForeignKey(pa => pa.AssetId);
-        }
-    }
+                     // One-to-Many: Asset to UserAssets
+                     builder.HasMany(a => a.UserAssets)
+                            .WithOne(ua => ua.Asset)
+                            .HasForeignKey(ua => ua.AssetId)
+                            .OnDelete(DeleteBehavior.Cascade);
+
+
+                     // One-to-Many: Asset to PostAssets
+                     builder.HasMany(a => a.PostAssets)
+                            .WithOne(pa => pa.Asset)
+                            .HasForeignKey(pa => pa.AssetId);
+              }
+       }
 }
