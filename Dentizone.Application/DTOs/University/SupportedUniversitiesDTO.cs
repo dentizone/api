38364461e7f72583e7928f12abe2ed4574ab61d2
Nouran <nourantamer077@gmail.com>
2025-06-08 18:00:57 +0300

    uni Endpoint

--- Dentizone.Application/DTOs/University/SupportedUniversitiesDTO.cs
+++ Dentizone.Application/DTOs/University/SupportedUniversitiesDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dentizone.Application.DTOs.University
 {
-    internal class SupportedUniversitiesDto : UniversityDto
+    public class SupportedUniversitiesDto : UniversityDto
     {
         public string Id { get; set; }
     }
