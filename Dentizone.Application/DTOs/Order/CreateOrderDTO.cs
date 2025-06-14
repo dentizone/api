e55a3af0dc5a09d43e65854f1eec5bd84fc9f091
Yara Nagy <yara1998@live.com>
2025-06-15 01:18:47 +0300

    order service in progress

---
+++ Dentizone.Application/DTOs/Order/CreateOrderDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dentizone.Domain.Entity;
+using FluentValidation;
+
+namespace Dentizone.Application.DTOs.Order
+{
+    public class CreateOrderDTO
+    {
+
+        public List<string> PostIds { get; set; } = new List<string>();
+
+    }
+    public class CreateOrderDTOValidation : AbstractValidator<CreateOrderDTO>
+    {
+        public CreateOrderDTOValidation()
+        {
+           RuleFor(static x => x.PostIds)
+                .NotEmpty().WithMessage("Posts list cannot be empty.")
+                .Must(posts => posts.All(post => post != null)).WithMessage("All posts must be valid.");
+        }
+    }
+}
