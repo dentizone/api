97d5b841dc971d7991688fa138ad3d477f4049de
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-15 01:25:26 +0300

    Add IOrderRepository interface and OrderRepository class
    Introduces the IOrderRepository interface with methods for updating and retrieving orders. Implements the OrderRepository class, which provides asynchronous methods for CRUD operations and supports pagination, filtering, and ordering of order entities.

---
+++ Dentizone.Domain/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Dentizone.Domain.Entity;
+using Dentizone.Domain.Interfaces.Repositories;
+
+namespace Dentizone.Infrastructure.Repositories;
+
+public interface IOrderRepository : IBaseRepo<Order>
+{
+    Task<Order> UpdateAsync(Order entity);
+
+    Task<IEnumerable<Order>> GetAllAsync(int page, Expression<Func<Order, bool>> filter,
+                                         Expression<Func<Order, object>> orderBy,
+                                         Expression<Func<Order, object>>[] includes = null);
+}
---
+++ Dentizone.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,61 @@
+﻿using Dentizone.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Dentizone.Infrastructure.Repositories
+{
+    public class OrderRepository(AppDbContext dbContext) : AbstractRepository(dbContext), IOrderRepository
+    {
+        public async Task<Order?> GetByIdAsync(string id)
+        {
+            return await dbContext.Orders
+                                  .FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task<Order> UpdateAsync(Order entity)
+        {
+            dbContext.Orders.Update(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Order> CreateAsync(Order entity)
+        {
+            await dbContext.Orders.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Order?> FindBy(Expression<Func<Order, bool>> condition,
+                                         Expression<Func<Order, object>>[]? includes = null)
+        {
+            IQueryable<Order> query = dbContext.Orders;
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(condition);
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync(int page, Expression<Func<Order, bool>> filter,
+                                                          Expression<Func<Order, object>> orderBy,
+                                                          Expression<Func<Order, object>>[] includes = null)
+        {
+            IQueryable<Order> query = dbContext.Orders.Where(filter);
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            query = query.OrderBy(orderBy).Skip(CalculatePagination(page)).Take(DefaultPageSize);
+            return await query.ToListAsync();
+        }
+    }
+}
