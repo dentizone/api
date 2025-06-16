fdcd113d9634400f328c92f60a331e938b1cd7f3
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-16 13:10:52 +0300

    denti

--- Dentizone.Presentaion/Program.cs
+++ Dentizone.Presentaion/Program.cs
@@ -4,6 +4,7 @@ using Dentizone.Infrastructure;
 using Dentizone.Infrastructure.DependencyInjection;
 using Dentizone.Infrastructure.Filters;
 using Dentizone.Infrastructure.Identity;
+using Dentizone.Infrastructure.Persistence.Seeder;
 using Dentizone.Presentaion.Context;
 using Dentizone.Presentaion.Extensions;
 using Dentizone.Presentaion.Middlewares;
