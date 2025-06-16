ea78b911e639c537662b0bcbfe9147678f6f51c8
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-16 22:09:58 +0300

    Refactor IOrderRepository: Update method signatures
    - Rearranged using directives for clarity.
    - Made `orderBy` and `includes` parameters nullable in `GetAllAsync`, allowing them to be optional.

--- Dentizone.Domain/Interfaces/Repositories/IOrderRepository.cs
+++ Dentizone.Domain/Interfaces/Repositories/IOrderRepository.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq.Expressions;
-using Dentizone.Domain.Entity;
+﻿using Dentizone.Domain.Entity;
+using System.Linq.Expressions;
 
 namespace Dentizone.Domain.Interfaces.Repositories;
 
@@ -8,6 +8,6 @@ public interface IOrderRepository : IBaseRepo<Order>
     Task<Order> UpdateAsync(Order entity);
 
     Task<IEnumerable<Order>> GetAllAsync(int page, Expression<Func<Order, bool>> filter,
-                                         Expression<Func<Order, object>> orderBy,
-                                         Expression<Func<Order, object>>[] includes = null);
+                                         Expression<Func<Order, object>>? orderBy = null,
+                                         Expression<Func<Order, object>>[]? includes = null);
 }
