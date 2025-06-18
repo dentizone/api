3191a17bc3a27ec0a46d9aba48e0fd10c54d5f10
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-18 18:51:43 +0300

    Remove UniversityStudentCountDto class
    The `UniversityStudentCountDto` class has been removed from the `Dentizone.Application.DTOs.Analytics` namespace. This class included properties for `UniversityName` and `StudentCount`, which are no longer part of the codebase.

--- Dentizone.Application/DTOs/Analytics/UniversityStudentCountDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dentizone.Application.DTOs.Analytics
-{
-    public class UniversityStudentCountDto
-    {
-        public string UniversityName { get; set; }
-        public int StudentCount { get; set; }
-    }
-}
