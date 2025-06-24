6e53abec8854de54c575b03ef329cd7bffd14d62
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-24 20:58:26 +0300

    Add IsReviewed property to Order and restructure DB
    - Introduced `IsReviewed` property in `Order` entity with default value `false`.
    - Created migration `OptmizeOrder` to add `IsReviewed` column to `Orders` table.
    - Updated `AppDbContextModelSnapshot` to include new property.
    - Created migration `OptmizeOrder2` to drop multiple tables, indicating a significant schema change.
    - Updated `Designer.cs` files for both migrations to reflect the changes.

---
+++ Dentizone.Infrastructure/Migrations/20250624175713_OptmizeOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dentizone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptmizeOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReviewed",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReviewed",
+                table: "Orders");
+        }
+    }
+}
