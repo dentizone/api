0804c5dc7ababde1db95a1b5f6cb451520d61bd9
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-25 17:10:08 +0300

    Enhance order retrieval with filtering capabilities
    Updated IOrderService and related classes to support
    filtering in order retrieval. Modified GetOrders method
    to accept FilterOrderDto for flexible querying.
    Implemented filtering logic in OrderService and
    OrderRepository. Added new FilterOrderDto class
    to encapsulate filter criteria.

---
+++ Dentizone.Application/DTOs/Order/FilterOrderDto.cs
@@ -0,0 +1,12 @@
+﻿using Dentizone.Domain.Enums;
+
+namespace Dentizone.Application.DTOs.Order
+{
+    public class FilterOrderDto
+    {
+        public string? OrderId { get; set; }
+        public string? BuyerName { get; set; }
+
+        public OrderStatues? Status { get; set; }
+    }
+}
