8eac81d01aaeb60f385fb56dff8155f2513610d4
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-29 20:52:24 +0300

    Refactor mapping configurations in AnswerProfile
    Updated the mapping configurations in the `AnswerProfile` class to use simplified type names instead of fully qualified names. This change improves code readability and reduces verbosity while retaining the existing mapping functionality for `CreateAnswerDto`, `UpdateAnswerDto`, and `AnswerViewDto`.

--- Dentizone.Application/AutoMapper/Answers/AnswerProfile.cs
+++ Dentizone.Application/AutoMapper/Answers/AnswerProfile.cs
@@ -6,15 +6,15 @@ namespace Dentizone.Application.AutoMapper.Answers
     {
         public AnswerProfile()
         {
-            CreateMap<Dentizone.Application.DTOs.Q_A.AnswerDTO.CreateAnswerDto, Dentizone.Domain.Entity.Answer>()
+            CreateMap<DTOs.Q_A.AnswerDTO.CreateAnswerDto, Domain.Entity.Answer>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString()))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ReverseMap();
-            CreateMap<Dentizone.Application.DTOs.Q_A.AnswerDTO.UpdateAnswerDto, Dentizone.Domain.Entity.Answer>()
+            CreateMap<DTOs.Q_A.AnswerDTO.UpdateAnswerDto, Domain.Entity.Answer>()
                 .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text))
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ReverseMap();
-            CreateMap<Dentizone.Application.DTOs.Q_A.AnswerDTO.AnswerViewDto, Dentizone.Domain.Entity.Answer>()
+            CreateMap<DTOs.Q_A.AnswerDTO.AnswerViewDto, Domain.Entity.Answer>()
                 .ReverseMap();
         }
     }
