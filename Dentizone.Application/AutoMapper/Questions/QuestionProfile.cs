4313c3dca380867b843f28d3aefa154e059de493
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-29 19:11:19 +0300

    Refactor AutoMapper configurations in QuestionProfile
    Updated mapping configurations in `QuestionProfile.cs` to simplify DTO references and improve clarity. Adjusted member mappings for `CreateQuestionDto`, `QuestionViewDto`, and `UpdateQuestionDto`, including changes to how user information is handled and ensuring proper mapping for new properties.

--- Dentizone.Application/AutoMapper/Questions/QuestionProfile.cs
+++ Dentizone.Application/AutoMapper/Questions/QuestionProfile.cs
@@ -7,17 +7,18 @@ namespace Dentizone.Application.AutoMapper.Questions
     {
         public QuestionProfile()
         {
-            CreateMap<Dentizone.Application.DTOs.Q_A.QuestionDTO.CreateQuestionDto, Dentizone.Domain.Entity.Question>()
+            CreateMap<CreateQuestionDto, Domain.Entity.Question>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString()))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ReverseMap();
-            CreateMap<Dentizone.Application.DTOs.Q_A.QuestionDTO.QuestionViewDto, Dentizone.Domain.Entity.Question>()
+            CreateMap<QuestionViewDto, Domain.Entity.Question>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.AskerName))
+                .ForMember(dest => dest.User, opt => opt.Ignore())
                 .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ReverseMap();
-            CreateMap<UpdateQuestionDto, Dentizone.Domain.Entity.Question>()
+                .ReverseMap()
+                .ForMember(dest => dest.AskerName, opt => opt.MapFrom(src => src.User != null ? src.User.FullName : string.Empty));
+            CreateMap<UpdateQuestionDto, Domain.Entity.Question>()
                 .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text))
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ReverseMap();
